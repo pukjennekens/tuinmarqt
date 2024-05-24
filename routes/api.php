b963ba34b1d1d89e9b39ac80ad4eadcd416ecf0f
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-05-24 14:28:00 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,18 @@ use App\Jobs\ExportArticles;
 use App\Jobs\ImportArticleGroups;
 use App\Jobs\ImportArticles;
 use App\Models\Setting;
-
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/webhook', [WebhookController::class, 'handle'])
     ->middleware(EnsureWooCommerceWebhookSignatureValid::class);
 
 Route::get('/test', function() {
+    Log::info('test', [
+        Setting::get('woocommerce_website_url'),
+        Setting::get('woocommerce_consumer_key'),
+        Setting::get('woocommerce_consumer_secret'),
+    ]);
     $products = WooCommerce::getClient()->get('products');
     return [
         Setting::get('woocommerce_website_url'),
